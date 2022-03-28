2a46875638e1d1373ee413aee1f06cde242775dd
shreyab1904 <shreyaborkar0201@gmail.com>
2022-03-28 17:38:19 +0530

    updated

--- test_purchase.py
+++ test_purchase.py
@@ -1,7 +1,13 @@
 import pytest
 
-def test_AddItemtoCart():
+@pytest.fixture
+def SetUp():
+    print("setup started")
+    yield
+    print("exited")
+
+def test_AddItemtoCart(SetUp):
     print("Added successfully")
 
-def test_RemoveItemfromCart():
+def test_RemoveItemfromCart(SetUp):
     print("Removed successfully")
